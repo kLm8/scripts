ce564a2f0e9a690130c1f732bf4354a6a0141bb8
Clement <gossart.clement@gmail.com>
2015-07-02 15:43:34 +0200

    improve: ex.sh

--- ex.sh
+++ ex.sh
@@ -4,6 +4,7 @@ docker stop test
 docker rm test
 cd /home/gossart/camomile-web-frontend
 git pull
+docker rmi web-dev
 docker build -t web-dev .
 docker run -d -p 8080:8070 -e CAMOMILE_API=http://vmjoker:32772 -e CAMOMILE_LOGIN=admin -e CAMOMILE_PASSWORD=p455w0rd --name test web-dev
 
