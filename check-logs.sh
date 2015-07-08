484c5721307ed3f579e175166bb01fd9602cadf1
Clement <gossart.clement@gmail.com>
2015-07-08 17:34:56 +0200

    test

--- check-logs.sh
+++ check-logs.sh
@@ -14,6 +14,7 @@ trap Maid INT
 
 # SSH into the camomile-dev docker container, create /tmp/check-logs.sh and execute it
 docker exec -it camomile-dev script /dev/null -c "'\
+ls \
 cat <<"EOF" > /tmp/check-logs.sh
 #!/bin/bash
 select FILENAME in "/app/log"/*
