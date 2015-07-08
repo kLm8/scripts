f56f999f85a11b654355ba43324a537e481c99ac
Clement <gossart.clement@gmail.com>
2015-07-08 12:20:42 +0200

    fix

--- check-logs.sh
+++ check-logs.sh
@@ -13,10 +13,10 @@ Maid() {
 trap Maid INT
 
 # Create a container and start a Bash session
-docker run --name camomile-dev_bash --rm -it camomile-dev bash
+# docker run --name camomile-dev_bash --rm -it camomile-dev bash
 
 # Create /tmp/check-logs.sh inside the running container
-docker exec -d camomile-dev_bash echo "
+docker exec -d camomile-dev echo "
 #!/bin/bash
 select FILENAME in "/app/log"/*
 do
@@ -32,7 +32,7 @@ do
 	esac
 done
 exit
-"
+" > /tmp/check-logs.sh
 
 # SSH into camomile-dev container
 docker exec -it camomile-dev_bash bash
