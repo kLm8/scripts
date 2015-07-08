77760c27215ffa33a9183f372c1ef55b22854a9b
Clement <gossart.clement@gmail.com>
2015-07-08 18:08:43 +0200

    fix: typo

--- check-logs.sh
+++ check-logs.sh
@@ -31,4 +31,4 @@ exit
 EOF
 
 # SSH into the camomile-dev docker container, create /tmp/check-logs.sh and execute it
-docker exec -it camomile-dev bach -c 'echo $script > /tmp/check-logs.sh; /bin/bash /tmp/check-logs.sh'
+docker exec -it camomile-dev bash -c 'echo $script > /tmp/check-logs.sh; /bin/bash /tmp/check-logs.sh'
