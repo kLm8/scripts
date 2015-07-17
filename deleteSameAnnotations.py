41fb7a0b0cf49bff177ea595754ff220deb6423e
Clement <gossart.clement@gmail.com>
2015-07-17 15:30:06 +0200

    debug: tests

--- deleteSameAnnotations.py
+++ deleteSameAnnotations.py
@@ -56,7 +56,7 @@ if __name__ == '__main__':
         # print datas[0]
 
         tmp = zip(fragments, ids, datas)
-        cleaned = set(sorted(tmp))
+        cleaned = [dict(t) for t in set([d for d in annotations])]
         print len(cleaned)
 
         # cleaned = [dict(t) for t in set([tuple(d.items()) for d in annotations])]
