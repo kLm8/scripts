4000a4f077477e023d185e7acc7c45178d8da096
Clement <gossart.clement@gmail.com>
2015-07-17 15:58:36 +0200

    debug: tuples

--- deleteSameAnnotations.py
+++ deleteSameAnnotations.py
@@ -39,7 +39,7 @@ if __name__ == '__main__':
         annotations = client.getAnnotations(l._id)
         print('annotations : %d' % len(annotations))
 
-        tmp = [tuple(a['fragment'].items()) + tuple(tuple([u'_id'],) + tuple(a['_id'],)) + tuple(tuple([u'data'],) + tuple(a['data'],)) for a in annotations]
+        tmp = [tuple(a['fragment'].items()) + (u'_id', a['_id']) + (u'data', a['data']) for a in annotations]
 
         # for i in range(0, len(annotations)):
         #     print tuple(annotations[i]['fragment'].items())
